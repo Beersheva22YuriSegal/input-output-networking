7539ab38f52864f8cc7d4bb2b6ffb8417cc77429
YuriSegal <yurisegal8@gmail.com>
2023-03-07 07:25:14 +0200

    CW#36

---
+++ src/telran/io/test/InputOutput.java
@@ -0,0 +1,14 @@
+package telran.io.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InputOutput {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
